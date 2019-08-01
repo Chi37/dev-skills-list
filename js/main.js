7516e87cdc53441c62557013e704cbe37e5dba7c
Chi <contact@chihuynh.me>
2019-07-31 22:15:57 -0700

    changed form to div because list wouldn't update with form tag

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
 /*----- constants -----*/
-let devSkillsArr = [];
+
 
 
 
  /*----- app's state (variables) -----*/ 
 
+let skillsArr = [];
 
 
 
+$('#submit-btn').on('click', function(e){
+    
+    var skill = $('#submitted-skill').val();
+    console.log(skill);
+    var listItem = `
+    <li><button class='removeBtn'>x</button>${skill}</li>
+    `
+    console.log(listItem)
+    $('ul').append(listItem);
+    addToSkillArr(skill);
+});
+
+
+//cacheing skills, I think
+
+function addToSkillArr (skill) {
+    skillsArr.push(skill);
+}
  
-/*----- cached element references -----*/
-
-
 
 
 /*----- event listeners -----*/ 
+// grab string from input box and store in variable
+
+// wait for uesr to click and add the string to the list 
 
 
 
-/*----- functions -----*/
+
